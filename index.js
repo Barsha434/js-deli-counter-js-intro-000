82d3cd943670d369c85d8ec6ae1b2f19706445ce
Barsha Dahal <dahalbarsha19@gmail.com>
2020-07-10 01:07:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ function nowServing(currentLine){
 var firstCustomer=currentLine.shift();
 return `Currently serving ${firstCustomer}.`
 }
+
 function currentLine(currentLineofPeople){
   if (currentLineofPeople.length===0){
   return `The line is currently empty.`
 }
 for (let i=0; i<currentLineofPeople.length;i++){
-  return `The line is currently: ${i+1}. currentLineofPeople[i]`
+  return `The line is currently: ${i+1}. ${currentLineofPeople[i]}`
 }
 }
