41d13771d7807662eaf90b2d1f440574b0384c4a
Barsha Dahal <dahalbarsha19@gmail.com>
2020-07-10 01:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function currentLine(currentLineofPeople){
   return `The line is currently empty.`
 }
 for (let i=0;i<currentLineofPeople.length;i++)
-  return `The line is currently: ${i+1}. ${currentLineofPeople[i++]}`
+  return `The line is currently: ${i+1}. ${currentLineofPeople[i]}`
+  i++;
 
 }
