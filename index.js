8a2f24363754778a8054bbc733af95d3434ee31e
Barsha Dahal <dahalbarsha19@gmail.com>
2020-07-10 01:47:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(currentLineofPeople){
 }
 var nameandNumber=[];
 for (let i=0;i<currentLineofPeople.length;i++){
-  nameandNumber.push(`${i+1}. ${currentLineofPeople[i]}`
+  nameandNumber.push(`${i+1}. ${currentLineofPeople[i]}`)
 }
 return `The line is currently: ${nameandNumber}`
 }
